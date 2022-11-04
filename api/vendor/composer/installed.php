4be6c72ff4e2ab2a85d3e93827604b2363478bec
PeterNdomano <peterndomano@gmail.com>
2022-11-04 14:28:32 +0300

    setting up composer.json and installing braintree php SDK

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '863bf1bded9acb8e6b7fed936d421a28ef4aaaaf',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '863bf1bded9acb8e6b7fed936d421a28ef4aaaaf',
+            'dev_requirement' => false,
+        ),
+        'braintree/braintree_php' => array(
+            'pretty_version' => '6.9.1',
+            'version' => '6.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../braintree/braintree_php',
+            'aliases' => array(),
+            'reference' => 'e7f0b691e6b0b39bf5dad703d955d0318a67c367',
+            'dev_requirement' => false,
+        ),
+    ),
+);
