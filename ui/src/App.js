e72f4372e3756a2a24628b340f243a3a04dfbc41
PeterNdomano <peterndomano@gmail.com>
2022-11-04 15:26:55 +0300

    creating methods

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dropin from "braintree-web-drop-in";
 
 function App() {
+  const [ error, setError ] = useState('');
   const [ showDropIn, setShowDropIn ] = useState(false);
+  const [ braintree, setBraintree ] = useState(null);
+  const [ dropInReady, setDropInReady ] = useState(false);
+  const [ loading, setLoading ] = useState(false);
+
+  const triggerPayment = () => {
+    //this triggers dropin to show
+    setShowDropIn(true);
+  }
+
+  const processPayment = () => {
+    //this handles and submits payment to the server
+    if(dropInReady && !loading) {//limit execution only when dropin is shown and there's no loading
+
+    } else {
+      setError('Please wait....')
+    }
+  }
+
+  const initializeBraintree = () => {
+    dropin.create({
+      authorization: "sandbox_6mrf6nhg_fknw3nxdc3cqnqy6", //braintree tokeniization
+      container: '#braintreeView', //id for the div where dropin will be loaded
+    }, function( error, instance ) {
+      if(error) {
+        setError(error.message);
+      }
+      else {
+        setDropInReady(true);
+        setBraintree(instance);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if(showDropIn) {
+      if(braintree) {
+        //destroy the old braintree instance then create new
+        braintree.teardown().then(() => {
+          initializeBraintree();
+        })
+      }
+      else {
+        initializeBraintree();
+      }
+    }
+
+  }, [ showDropIn ]);
   return (
     <div className="container">
       <div className="row">
@@ -19,9 +67,19 @@ function App() {
         </div>
 
         <div className="col-md-6 p-4">
-          <button className="btn btn-primary btn-pay">
-            Pay With By Credit Card
-          </button>
+          <div style={{ width: "100%" }} id="braintreeView">
+          </div>
+
+          {
+            (!showDropIn) ?
+            <button onClick={() => triggerPayment()} className="btn btn-primary btn-pay">
+              Pay With By Credit Card
+            </button>
+            :
+            <button onClick={() => processPayment()} className="btn btn-primary btn-pay">
+              {(!dropInReady || loading) ? "Please wait...." : "Pay Now" }
+            </button>
+          }
         </div>
 
       </div>
