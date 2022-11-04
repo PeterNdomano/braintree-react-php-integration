53e4befbe2ddc768dc2fd12b2c86aa3175416537
PeterNdomano <peterndomano@gmail.com>
2022-11-04 15:01:44 +0300

    basic UI design

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
 import dropin from "braintree-web-drop-in";
 
 function App() {
+  const [ showDropIn, setShowDropIn ] = useState(false);
   return (
     <div className="container">
-      <button className="btn primary">Test</button>
+      <div className="row">
+
+        <div className="col-md-12 p-4">
+          <h1 className="heading">React + PHP</h1>
+          <h3 className="text-muted">Complete Example on Braintree Payment Gateway Integration</h3>
+          <hr/>
+        </div>
+
+        <div className="col-md-6 p-4">
+          <h6 className="text-muted">Price</h6>
+          <h1>25.00 <span style={{ fontSize: "15px" }}>USD</span></h1>
+        </div>
+
+        <div className="col-md-6 p-4">
+          <button className="btn btn-primary btn-pay">
+            Pay With By Credit Card
+          </button>
+        </div>
+
+      </div>
     </div>
   );
 }
